86f4675e476979faa18a7dcb09129f170292091b
Edson Shideki Kokado <eskokado@gmail.com>
2018-02-12 09:09:09 -0200

    Correcoes: update em CategoriaService e contructor de Pagamento

--- src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pagamento.java
@@ -34,7 +34,9 @@ public abstract class Pagamento implements Serializable {
 	}
 
 	public Pagamento(Integer id, EstadoPagamento estado, Pedido pedido) {
+
 		super();
+		
 		this.id = id;
 		this.estado = (estado==null) ? null : estado.getCod();
 		this.pedido = pedido;
