042943a363ccebfd06f110336897da4b4478eb27
Edson Shideki Kokado <eskokado@gmail.com>
2018-02-13 10:01:54 -0200

    Subtotal e total do pedido

--- src/main/java/com/nelioalves/cursomc/domain/Pedido.java
+++ src/main/java/com/nelioalves/cursomc/domain/Pedido.java
@@ -58,6 +58,14 @@ public class Pedido implements Serializable {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public double getValorTotal() {
+		double soma = 0.0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+	
 	public List<Produto> getProdutos() {
 		List<Produto> lista = new ArrayList<>();
 		for(ItemPedido x : itens) {
